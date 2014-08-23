d9d76563cc708eeb8f7cf8e84071502a62c08420
calebbfmv <tscalebbfmv@hotmail.com>
2014-08-23 17:41:16 -0500

    Commit

--- src/main/java/org/arkham/cs/db/Authentication.java
+++ src/main/java/org/arkham/cs/db/Authentication.java
@@ -2,10 +2,10 @@ package org.arkham.cs.db;
 
 public class Authentication {
 	
-    public static String sqlhost = "72.8.130.42";
+    public static String sqlhost = "<snip>";
     public static String sqldb = "arkham_cosmetics";
     public static String sqluser = "root"; // TODO: Replace w/ slave account, setup perms.
-    public static String sqlpass = "oEtXfeo0AzpCbPG05QkumNA67IafKrTs4mz0";
+    public static String sqlpass = "<snip>";
     public static int sqlport = 3306;
     public static String sqlurl = "jdbc:mysql://" + sqlhost + ":" + sqlport + "/" + sqldb;
 
